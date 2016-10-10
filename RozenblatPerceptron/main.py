6e7991395a4251c3cc6095c39825754557ac7428
Vrublevskiy Vitaliy <vrublevskiy@lun.ua>
2016-10-10 18:35:49 +0300

    add tests, main code for perceptron and main prog

---
+++ RozenblatPerceptron/main.py
@@ -0,0 +1,12 @@
+from RozenblatPerceptron.test_bipolar import *
+from RozenblatPerceptron.perceptron import *
+
+
+if __name__ == '__main__':
+
+    t1 = alphabet[u'В']
+    t2 = alphabet[u'Р']
+
+    perceptron = RozenblatPerceptronOneR(len(t1), 2)
+
+    t = 0
---
+++ RozenblatPerceptron/perceptron.py
@@ -0,0 +1,32 @@
+from random import random
+
+
+class RozenblatPerceptronOneR:
+    def __init__(self, s_number, a_number):
+
+        self.s_number = s_number
+        self.a_number = a_number
+        self.r_number = 1
+
+        self.init_sa_matrix()
+        self.init_ar_matrix()
+
+    def init_sa_matrix(self):
+        self.sa_matrix = []
+
+        for i in range(self.s_number):
+            new = []
+            for j in range(self.a_number):
+                new.append(random())
+            self.sa_matrix.append(new)
+
+    def init_ar_matrix(self):
+        self.ar_matrix = []
+
+        for i in range(self.s_number):
+            new = []
+            for j in range(self.a_number):
+                new.append(0)
+            self.ar_matrix.append(new)
+
+
